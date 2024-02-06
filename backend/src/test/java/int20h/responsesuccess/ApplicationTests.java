24669bbc0871dfebac2db0ba92b85d382e44ce06
Dmytro Hrydin <dmitriygridin0904@gmail.com>
2024-02-06 12:00:45 +0100

    Initial commit

---
+++ backend/src/test/java/int20h/responsesuccess/ApplicationTests.java
@@ -0,0 +1,13 @@
+package int20h.responsesuccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
