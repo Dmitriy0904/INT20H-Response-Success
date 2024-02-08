49a41aaa297d916e57b35614995ead14169a9f1f
Dmytro Hrydin <dmitriygridin0904@gmail.com>
2024-02-08 18:20:08 +0100

    Updated script

--- backend/src/main/resources/db/migration/V1__init.sql
+++ backend/src/main/resources/db/migration/V1__init.sql
@@ -12,19 +12,13 @@ CREATE TABLE IF NOT EXISTS auctions (
     id            UUID          PRIMARY KEY DEFAULT uuid_generate_v4(),
     name          VARCHAR(100)  NOT NULL,
     description   TEXT          NOT NULL,
+    user_id       UUID          NOT NULL,
     photo_key     VARCHAR(100)  NOT NULL,
     status        VARCHAR(100)  NOT NULL,
     start_price   REAL          NOT NULL,
     actual_price  REAL          NOT NULL,
-    created_at    TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE IF NOT EXISTS user_auctions (
-    id           UUID   PRIMARY KEY DEFAULT uuid_generate_v4(),
-    user_id      UUID   NOT NULL,
-    auction_id   UUID   NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (id),
-    FOREIGN KEY (auction_id) REFERENCES auctions (id)
+    created_at    TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
 CREATE TABLE IF NOT EXISTS bids (
